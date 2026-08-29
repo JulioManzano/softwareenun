8d0b364b3f5be1c72dd086e0316bbe2fe5573aff
“Julio <juliomanzano1996@gmail.com>
2026-08-29 01:03:03 -0300

    test print

--- core/scheme.py
+++ core/scheme.py
@@ -2,6 +2,8 @@ import graphene
 
 from graphene_django_extras import DjangoListObjectField
 
+from core.models import PublicFile
+
 from .types import (
     PublicFileListType,
     PublicFileProjectListType,
@@ -19,6 +21,16 @@ class Query(graphene.ObjectType):
         PublicFileListType,
         filterset_class=PublicFileFilter,
     )
+
+    def resolve_public_files(self, info, **kwargs):
+        print("🔥🔥🔥 ENTRÓ A resolve_public_files")
+        print("ARGS:", kwargs)
+
+        queryset = PublicFile.objects.all()
+
+        print("TOTAL:", queryset.count())
+
+        return queryset
 
     public_file_projects = DjangoListObjectField(
         PublicFileProjectListType,
