d2676bbe35b680a20b2a68d586b7812c0a7d5a3c
“Julio <juliomanzano1996@gmail.com>
2026-08-29 00:37:18 -0300

    fx m,bs

--- core/filters.py
+++ core/filters.py
@@ -10,30 +10,68 @@ class PublicFileFilter(django_filters.FilterSet):
     )
 
     project_slug = django_filters.CharFilter(
-        field_name="project__slug"
+        field_name="project__slug",
+        lookup_expr="iexact",
     )
 
     slug = django_filters.CharFilter(
-        field_name="slug"
+        lookup_expr="iexact",
+    )
+
+    ordering = django_filters.CharFilter(
+        method="filter_ordering"
     )
 
     class Meta:
         model = PublicFile
-        fields = (
+        fields = [
+            "id",
             "project_id",
             "project_slug",
             "slug",
-        )
+            "ordering",
+        ]
+
+    def filter_ordering(self, queryset, name, value):
+        allowed_fields = [
+            "name", "-name",
+            "created_at", "-created_at",
+            "slug", "-slug",
+        ]
+
+        if value in allowed_fields:
+            return queryset.order_by(value)
+
+        return queryset.order_by("-created_at")
 
 
 class PublicFileProjectFilter(django_filters.FilterSet):
 
     slug = django_filters.CharFilter(
-        field_name="slug"
+        lookup_expr="iexact",
+    )
+
+    ordering = django_filters.CharFilter(
+        method="filter_ordering"
     )
 
     class Meta:
         model = PublicFileProject
-        fields = (
+        fields = [
+            "id",
+            "name",
             "slug",
-        )
+            "ordering",
+        ]
+
+    def filter_ordering(self, queryset, name, value):
+        allowed_fields = [
+            "name", "-name",
+            "created_at", "-created_at",
+            "slug", "-slug",
+        ]
+
+        if value in allowed_fields:
+            return queryset.order_by(value)
+
+        return queryset.order_by("name")
--- core/types.py
+++ core/types.py
@@ -1,55 +1,29 @@
-import graphene
 from graphene_django import DjangoObjectType
 from graphene_django_extras import DjangoListObjectType
+from graphene_django_extras.paginations import LimitOffsetGraphqlPagination
 
 from .models import PublicFile, PublicFileProject
-
-
-class PublicFileType(DjangoObjectType):
-    class Meta:
-        model = PublicFile
-        fields = (
-            "id",
-            "project",
-            "file",
-            "name",
-            "slug",
-            "created_at",
-            "is_public",
-        )
 
 
 class PublicFileListType(DjangoListObjectType):
     class Meta:
         model = PublicFile
-        fields = (
-            "id",
-            "project",
-            "file",
-            "name",
-            "slug",
-            "created_at",
-            "is_public",
-        )
+        pagination = LimitOffsetGraphqlPagination(default_limit=25)
 
 
-class PublicFileProjectType(DjangoObjectType):
+class PublicFileType(DjangoObjectType):
     class Meta:
-        model = PublicFileProject
-        fields = (
-            "id",
-            "name",
-            "slug",
-            "created_at",
-        )
+        model = PublicFile
+        fields = "__all__"
 
 
 class PublicFileProjectListType(DjangoListObjectType):
     class Meta:
         model = PublicFileProject
-        fields = (
-            "id",
-            "name",
-            "slug",
-            "created_at",
-        )
+        pagination = LimitOffsetGraphqlPagination(default_limit=25)
+
+
+class PublicFileProjectType(DjangoObjectType):
+    class Meta:
+        model = PublicFileProject
+        fields = "__all__"
