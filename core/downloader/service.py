a077c44bedf4b854703217c23f687b148d6b4285
“Julio <juliomanzano1996@gmail.com>
2026-09-01 02:55:40 -0300

    add prints

--- core/downloader/service.py
+++ core/downloader/service.py
@@ -8,9 +8,21 @@ class DownloaderService:
 
         for provider in self.providers:
             try:
-                return provider.get_info(url)
+                data = provider.get_info(url)
+
+                print(
+                    f"DOWNLOAD PROVIDER SUCCESS: "
+                    f"{provider.__class__.__name__}"
+                )
+
+                return data
 
             except Exception as e:
+                print(
+                    f"DOWNLOAD PROVIDER ERROR: "
+                    f"{provider.__class__.__name__}: {e}"
+                )
+
                 errors.append(
                     f"{provider.__class__.__name__}: {str(e)}"
                 )
@@ -20,22 +32,13 @@ class DownloaderService:
             + " | ".join(errors)
         )
 
-    def get_download_url(self, url, format_id):
-        errors = []
-
-        for provider in self.providers:
-            try:
-                return provider.get_download_url(
-                    url,
-                    format_id,
-                )
-
-            except Exception as e:
-                errors.append(
-                    f"{provider.__class__.__name__}: {str(e)}"
-                )
-
-        raise Exception(
-            "No downloader provider available: "
-            + " | ".join(errors)
+    def get_download_url(
+        self,
+        provider,
+        url,
+        format_id,
+    ):
+        return provider.get_download_url(
+            url,
+            format_id,
         )
